6500cf0ebc0bcb0e1d024c19916180c65c8a14bb
Diwanshu <saidiwanshu1880@gmail.com>
2023-08-12 22:00:44 +0530

    fixed some error

--- Components/Resume/Resume.jsx
+++ Components/Resume/Resume.jsx
@@ -9,11 +9,11 @@ import {FcDownload} from 'react-icons/fc'
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 const ResumePDF = () => {
-  const [numPages, setNumPages] = useState<number>();
-  const [pageNumber, setPageNumber] = useState<number>(1);
+  const [numPages, setNumPages] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
   const [width, setwidth] = useState(0);
 
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+  function onDocumentLoadSuccess({ numPages }){
     setNumPages(numPages);
   }
 
@@ -40,6 +40,7 @@ const ResumePDF = () => {
       <a href="../../public/resume.pdf" className=" px-6 py-2 mx-auto bg-white flex items-center justify-center gap-2 text-black rounded-full mb-6" download={true}>Download <FcDownload/> </a>
       <a href={'/resume.pdf'} target="_blank" className=" px-6 py-2 mx-auto bg-white flex items-center justify-center gap-2 text-black rounded-full mb-6" > Open in new tab </a>
       </div>
+      
       {/* @ts-ignore */}
       <Document
         file="./resume.pdf"       
@@ -49,9 +50,6 @@ const ResumePDF = () => {
         {/* @ts-ignore */}
         <Page
           width={width > 600? width * 0.7: width * 0.9}
-          
-          // renderTextLayer={false}
-          // renderAnnotationLayer={false}
           pageNumber={pageNumber}
         />
       </Document>
