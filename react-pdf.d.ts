7097c84af6793c6d349c520213115af2fa465f56
Diwanshu <saidiwanshu1880@gmail.com>
2023-08-12 22:04:41 +0530

    deleted .d.ts

--- react-pdf.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "react-pdf" {
-    export const Document: React.ComponentType<any>;
-    export const Page: React.ComponentType<any>;
-    export const pdfjs: any; // You might need to adjust this based on what you're using from pdfjs
-  }
-  
